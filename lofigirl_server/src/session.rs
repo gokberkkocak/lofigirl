1414d7829d623fac7b3522e86b6dc34f757586c4
Gökberk KOÇAK <gk34@st-andrews.ac.uk>
2021-05-25 15:42:34 +0100

    Split tables and write queries to fetch as well

--- lofigirl_server/src/session.rs
+++ lofigirl_server/src/session.rs
@@ -15,88 +15,173 @@ impl TokenDB {
         Ok(token_db)
     }
 
-    pub async fn insert_new(
+    pub async fn get_or_generate_token(
         &self,
         lastfm: Option<LastFMConfig>,
         listenbrainz: Option<ListenBrainzConfig>,
-    ) -> Result<Uuid> {
+    ) -> Result<String> {
         (lastfm.is_some() && listenbrainz.is_some())
             .then(|| ())
             .ok_or(ConfigError::EmptyListeners)?;
         let mut conn = self.pool.acquire().await?;
-        let uuid = Uuid::new_v4();
-        let lastfm_fields = if let Some(l) = lastfm {
-            (
-                Some(l.api_key),
-                Some(l.api_secret),
-                Some(l.username),
-                Some(l.password),
-            )
-        } else {
-            (None, None, None, None)
-        };
-        let listenbrainz_field = if let Some(l) = listenbrainz {
-            Some(l.token)
+        let lfm_id = if let Some(lastfm) = lastfm {
+            Some(self.get_lastfm_id(&lastfm).await?)
         } else {
             None
         };
-        let token_string = uuid.to_hyphenated().to_string();
-        let _id = sqlx::query!(
-                r#"
-                    INSERT INTO tokens ( token, listenbrainz_token, lastfm_api_key, lastfm_api_secret, lastfm_username, lastfm_password  )
-                    VALUES ( ?1, ?2, ?3, ?4, ?5, ?6 )
-                "#,
-                token_string,
-                listenbrainz_field,
-                lastfm_fields.0,
-                lastfm_fields.1,
-                lastfm_fields.2,
-                lastfm_fields.3,
-            )
-            .execute(&mut conn)
-            .await?
-            .rows_affected();
+        let lb_id = if let Some(listenbrainz) = listenbrainz {
+            Some(self.get_listenbrainz_id(&listenbrainz).await?)
+        } else {
+            None
+        };
 
-        Ok(uuid)
+        let optional_token = sqlx::query!(
+            r#"
+                SELECT token FROM tokens WHERE lastfm_id = ?1 AND listenbrainz_id = ?2
+            "#,
+            lfm_id,
+            lb_id
+        )
+        .fetch_optional(&mut conn)
+        .await?;
+        match optional_token {
+            Some(rec) => Ok(rec.token),
+            None => {
+                let token = Uuid::new_v4();
+                let token_str = token.to_hyphenated().to_string();
+                let _id = sqlx::query!(
+                    r#"
+                        INSERT INTO tokens ( token, lastfm_id, listenbrainz_id )
+                        VALUES ( ?1, ?2, ?3)
+                    "#,
+                    token_str,
+                    lfm_id,
+                    lb_id
+                )
+                .execute(&mut conn)
+                .await?
+                .rows_affected();
+                Ok(token_str)
+            }
+        }
+    }
+
+    async fn get_lastfm_id(&self, lastfm: &LastFMConfig) -> Result<i64> {
+        let mut conn = self.pool.acquire().await?;
+        let optional_id = sqlx::query!(
+            r#"
+                SELECT id FROM lastfm WHERE username = ?1
+            "#,
+            lastfm.username
+        )
+        .fetch_optional(&mut conn)
+        .await?;
+        match optional_id {
+            Some(rec) => Ok(rec.id),
+            None => {
+                let id = sqlx::query!(
+                    r#"
+                        INSERT INTO lastfm (username, password, api_key, api_secret)
+                        VALUES ( ?1, ?2, ?3, ?4)
+                    "#,
+                    lastfm.username,
+                    lastfm.password,
+                    lastfm.api_key,
+                    lastfm.api_secret,
+                )
+                .execute(&mut conn)
+                .await?
+                .last_insert_rowid();
+                Ok(id)
+            }
+        }
+    }
+
+    async fn get_listenbrainz_id(&self, listenbrainz: &ListenBrainzConfig) -> Result<i64> {
+        let mut conn = self.pool.acquire().await?;
+        let optional_id = sqlx::query!(
+            r#"
+                SELECT id FROM listenbrainz WHERE token = ?1
+            "#,
+            listenbrainz.token
+        )
+        .fetch_optional(&mut conn)
+        .await?;
+        match optional_id {
+            Some(rec) => Ok(rec.id),
+            None => {
+                let id = sqlx::query!(
+                    r#"
+                        INSERT INTO listenbrainz ( token )
+                        VALUES ( ?1 )
+                    "#,
+                    listenbrainz.token,
+                )
+                .execute(&mut conn)
+                .await?
+                .last_insert_rowid();
+                Ok(id)
+            }
+        }
     }
 
     pub async fn get_info_from_token(
         &self,
-        token_uuid: Uuid,
+        token_str: &str,
     ) -> Result<(Option<LastFMConfig>, Option<ListenBrainzConfig>)> {
         let mut conn = self.pool.acquire().await?;
-        let token_string = token_uuid.to_hyphenated().to_string();
-        let res = sqlx::query!(
+        let rec = sqlx::query!(
             r#"
-                SELECT listenbrainz_token, lastfm_api_key, lastfm_api_secret, lastfm_username, lastfm_password FROM tokens
+                SELECT lastfm_id, listenbrainz_id FROM tokens
                 WHERE token = ?1
             "#,
-            token_string
+            token_str
         )
         .fetch_one(&mut conn)
         .await?;
-        let lb_config = if let Some(token) = res.listenbrainz_token {
-            Some(ListenBrainzConfig { token })
+        let lb_config = if let Some(id) = rec.listenbrainz_id {
+            Some(self.get_listenbrainz_config(id).await?)
         } else {
             None
         };
-        let lfm_config = match (
-            res.lastfm_api_key,
-            res.lastfm_api_secret,
-            res.lastfm_username,
-            res.lastfm_password,
-        ) {
-            (Some(api_key), Some(api_secret), Some(username), Some(password)) => {
-                Some(LastFMConfig {
-                    api_key,
-                    api_secret,
-                    username,
-                    password,
-                })
-            }
-            _ => None,
+        let lfm_config = if let Some(id) = rec.lastfm_id {
+            Some(self.get_lastfm_config(id).await?)
+        } else {
+            None
         };
-
         Ok((lfm_config, lb_config))
+    }
+
+    async fn get_listenbrainz_config(&self, id: i64) -> Result<ListenBrainzConfig> {
+        let mut conn = self.pool.acquire().await?;
+        let rec = sqlx::query!(
+            r#"
+                SELECT token FROM listenbrainz
+                WHERE id = ?1
+            "#,
+            id
+        )
+        .fetch_one(&mut conn)
+        .await?;
+        Ok(ListenBrainzConfig { token: rec.token })
+    }
+
+    async fn get_lastfm_config(&self, id: i64) -> Result<LastFMConfig> {
+        let mut conn = self.pool.acquire().await?;
+        let rec = sqlx::query!(
+            r#"
+                SELECT username, password, api_key, api_secret FROM lastfm
+                WHERE id = ?1
+            "#,
+            id
+        )
+        .fetch_one(&mut conn)
+        .await?;
+        Ok(LastFMConfig {
+            api_key: rec.api_key,
+            api_secret: rec.api_secret,
+            username: rec.username,
+            password: rec.password,
+        })
     }
 }
--- migrations/20210525000135_table.sql
+++ migrations/20210525000135_table.sql
@@ -1,10 +1,22 @@
 -- Add migration script here
 CREATE TABLE IF NOT EXISTS tokens (
+    id                           INTEGER PRIMARY KEY NOT NULL,
+    token                        TEXT NOT NULL,
+    lastfm_id                    INT,
+    listenbrainz_id              INT,
+    FOREIGN KEY(lastfm_id)       REFERENCES lastfm(id),
+    FOREIGN KEY(listenbrainz_id) REFERENCES listenbrainz(id)
+);
+
+CREATE TABLE IF NOT EXISTS lastfm (
     id          INTEGER PRIMARY KEY NOT NULL,
-    token       TEXT NOT NULL,
-    listenbrainz_token TEXT,
-    lastfm_api_key TEXT,
-    lastfm_api_secret TEXT,
-    lastfm_username TEXT,
-    lastfm_password TEXT
+    username    TEXT NOT NULL,
+    password    TEXT NOT NULL,
+    api_key     TEXT NOT NULL,
+    api_secret  TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS listenbrainz (
+    id          INTEGER PRIMARY KEY NOT NULL,
+    token       TEXT NOT NULL
 );
